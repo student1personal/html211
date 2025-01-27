bc49d0843f9a2fd96d00213da86a86deacf3606d
student1personal <162736955+student1personal@users.noreply.github.com>
2025-01-27 14:55:45 +0530

    Create script.js

---
+++ JS/code/events2/script.js
@@ -0,0 +1,8 @@
+const buttonEle = document.getElementById('btn');
+const gpEle = document.getElementById("gp")
+const pEle = document.getElementById("p")
+const cEle = document.getElementById("c")
+// function fun (e)
+// {
+//     console.log(e.target)
+// }
