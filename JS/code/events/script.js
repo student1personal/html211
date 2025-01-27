147fdafe7ad4d4de227d38117df4eaf73fe0faf4
student1personal <162736955+student1personal@users.noreply.github.com>
2025-01-27 14:52:33 +0530

    Create script.js

---
+++ JS/code/events/script.js
@@ -0,0 +1,19 @@
+// function fun (e)
+// {
+//     console.log(e.target)
+//     alert("hi")
+// }
+// function fun(e) {
+// 	console.log(`You pressed ${e.key} key`)
+// }
+// function fun2 (e)
+// {
+//     console.log(e.target.textContent)
+// }
+// document.addEventListener('keypress', fun);
+// document.getElementById('btn').addEventListener('click', fun2)
+/* when you press a, print a is presses*/
+/* print button text when you click button */
+
+
+
