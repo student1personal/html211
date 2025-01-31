d1892a891bcbf05d9e788f35fc7fcff658311ac6
student1personal <162736955+student1personal@users.noreply.github.com>
2025-01-31 11:13:15 +0530

    Create script.js

---
+++ JS/code/lib/script.js
@@ -0,0 +1,12 @@
+//jquery code is used for AJAX and DOM manipulations
+
+//$ means document.queryselector in jquery
+// $('#btn').click(function ()
+// {
+//     $('#box').slideToggle(800)
+// })
+$('#btn').click(function ()
+{
+    $('#box').text("Hello world");
+    $('#box').css('color', 'white');
+})
